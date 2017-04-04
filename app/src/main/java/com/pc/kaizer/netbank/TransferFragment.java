058290bbc4c229a3c10ddbc7356c1175c8169653
KaizerSoze <dhamapurkar54@gmail.com>
2017-04-04 22:34:48 +0530

    Issue fixed and transaction logging added.

--- app/src/main/java/com/pc/kaizer/netbank/TransferFragment.java
+++ app/src/main/java/com/pc/kaizer/netbank/TransferFragment.java
@@ -16,8 +16,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
