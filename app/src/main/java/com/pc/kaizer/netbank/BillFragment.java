30994068e300b7a1af415f4b822517de35b98932
786kaizersoze <dhamapurkar54@gmail.com>
2017-04-08 10:05:34 +0530

    Bill payment code added(complete)

--- app/src/main/java/com/pc/kaizer/netbank/BillFragment.java
+++ app/src/main/java/com/pc/kaizer/netbank/BillFragment.java
@@ -1,10 +1,13 @@
 package com.pc.kaizer.netbank;
 
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +23,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+import static com.pc.kaizer.netbank.R.style.Theme_AppCompat_Dialog;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,6 +42,8 @@ public class BillFragment extends Fragment {
     private Button submit;
     private DatabaseReference db;
     private String type ="";
+    private String amt;
+    Random r = new Random(System.currentTimeMillis());
     SharedPreferences settings;
     public BillFragment() {
         // Required empty public constructor
@@ -67,23 +78,25 @@ public class BillFragment extends Fragment {
 
     private void submit()
     {
-        String amt = ed2.getText().toString();
+        amt = ed2.getText().toString();
         View focus = null;
+        Log.d("3",type);
         boolean cancel=false;
-        if(!elec.isSelected() && !tel.isSelected() && !water.isSelected())
+        if(!elec.isEnabled() && !tel.isEnabled() && !water.isEnabled())
         {
             cancel = true;
+            focus = ed2;
             Toast.makeText(getActivity(),"Please select atleast one category",Toast.LENGTH_LONG);
         }
-        if(elec.isSelected())
+        if(elec.isEnabled())
         {
             type = "electricity";
         }
-        if(tel.isSelected())
+        if(tel.isEnabled())
         {
             type = "telephone";
         }
-        if(water.isSelected()) {
+        if(water.isEnabled()) {
             type = "water";
         }
         if(amt.isEmpty())
@@ -94,21 +107,35 @@ public class BillFragment extends Fragment {
         }
         if(cancel)
         {
+            Log.d("1",type);
             focus.requestFocus();
         }
         else
         {
-            db.child("users").child(type).addListenerForSingleValueEvent(new ValueEventListener() {
+            Log.d("GG",type);
+            db.child("users").child(settings.getString("uid","")).child(type).addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
-                    if(!dataSnapshot.getValue().equals(null))
-                    {
-                        if(dataSnapshot.getValue().toString().equals("Not assigned"))
+                        if(!dataSnapshot.getValue().toString().equals("Not assigned"))
                         {
                             db.child("accounts").child(settings.getString("acc_no","")).child("balance").addListenerForSingleValueEvent(new ValueEventListener() {
                                 @Override
                                 public void onDataChange(DataSnapshot dataSnapshot){
-                                    
+                                    float balance = Float.parseFloat(dataSnapshot.getValue().toString());
+                                    Log.d("gg",String.valueOf(balance));
+                                    balance = balance-Float.parseFloat(amt);
+                                    db.child("accounts").child(settings.getString("acc_no","")).child("balance").setValue(balance);
+                                    final String tid = String.valueOf((1 + r.nextInt(2)) * 10000 + r.nextInt(10000));
+                                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh:mm:ss");
+                                    String format = simpleDateFormat.format(new Date());
+                                    db.child("transactions").child(settings.getString("uid","")).child(tid).child("amount").setValue("-" + amt);
+                                    db.child("transactions").child(settings.getString("uid","")).child(tid).child("timestamp").setValue(format);
+                                    db.child("transactions").child(settings.getString("uid","")).child(tid).child("type").setValue("Bill: "+type);
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                                    builder.setTitle("Bill Pay alert");
+                                    builder.setMessage("Tera bill bhar diya bhai! Tension na le!");
+                                    AlertDialog alert = builder.create();
+                                    alert.show();
                                 }
 
                                 @Override
@@ -122,15 +149,10 @@ public class BillFragment extends Fragment {
                             Toast.makeText(getActivity(), "Please register your "+type+"customer number with the bank.", Toast.LENGTH_SHORT).show();
                         }
                     }
-                    else
-                    {
-
-                    }
-                }
 
                 @Override
                 public void onCancelled(DatabaseError databaseError) {
-
+                    Toast.makeText(getActivity(), "Something went wrong", Toast.LENGTH_SHORT).show();
                 }
             });
         }
